f02bafe23c9dafc92c396cd292ef0ad94ba9dcb1
Alessandro <alexficca15@gmail.com>
2023-06-04 15:35:15 +0200

    add run train

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 from argparse import ArgumentParser
-from models.MobileNetV2 import MobileNetV2
-from train import trainer
 import torch
 from random_search import search_random
-from dataset import get_data_loader 
 from evolution_search import search_evolution
-import argparse 
 
 
 
@@ -13,36 +9,22 @@ def main():
     
     parser = ArgumentParser()
     
-    parser.add_argument("--algo", type=str, default='ea_search', choices=("random_searc", "ea_search"))
+    parser.add_argument("--algo", type=str, default='random_search', choices=("random_search", "ea_search"))
     parser.add_argument('--max_flops', type=float, default=float('inf'))
     parser.add_argument('--max_params', type=float, default=float('inf'))
-    parser.add_argument('--n_random', type=int, default=100)
+    parser.add_argument('--n_random', type=int, default=10)
     parser.add_argument('--initial_pop', type=int, default=25)
     parser.add_argument('--generation_ea', type=int, default=100)
-    parser.add_argument("--save", type=bool, default=False)
+    parser.add_argument("--save", type=bool, default=True)
 
-    # parser.add_argument("--root_data", type=str, default="COCOdataset/all2017")
-    # parser.add_argument("--ann_train", type=str, default="MLDL23_NAS_Project/visualwakewords/instances_train.json")
-    # parser.add_argument("--ann_val", type=str, default="MLDL23_NAS_Project/visualwakewords/instances_val.json")
-    # parser.add_argument("--batch_size", type=int, default=64)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     
-    # batch size = 64, 96, 128 (possible error CUDA out of memory)
-        
-    # root_data = "COCOdataset/all2017"
-    # path_annotations_train = "MLDL23_NAS_Project/visualwakewords/instances_train.json"
-    # path_annotations_val = "MLDL23_NAS_Project/visualwakewords/instances_val.json"
-
-    # train_dataloader, val_dataloader, test_dataloader = get_data_loader(root_data,
-    #                                                                     path_annotations_train,
-    #                                                                     path_annotations_val,
-    #                                                                     64)
-    # inputs = next(iter(train_dataloader))
+    
     
     mini_batch_size = 8
-    inputs = torch.rand(mini_batch_size,3,224,224)
+    inputs = torch.rand(mini_batch_size,3,224,224).to(device)
 
     args = parser.parse_args()
     max_flops = args.max_flops
@@ -84,20 +66,10 @@ def main():
 
     
     if args.save:
-        model = best_models[0]
-        model_scripted = torch.jit.script(model) # Export to TorchScript
-        model_scripted.save('model_scripted.pt') # Save
+        model = best_models[0].get_model()
+        torch.save(model, 'model.pth')
     
     
-    # MV2_val_loss, MV2_val_accuracy, MV2_train_loss, MV2_train_accuracy = trainer(train_dataloader,
-    #                                                                              val_dataloader,
-    #                                                                              test_dataloader,
-    #                                                                              learning_rate=0.1,
-    #                                                                              weight_decay=0.000001,
-    #                                                                              momentum=0.9,
-    #                                                                              epochs=2,
-    #                                                                              model= model,
-    #                                                                              device=device )
     
                                                                                  
 
---
+++ src/run_train.py
@@ -0,0 +1,54 @@
+from argparse import ArgumentParser
+import torch
+
+from dataset import get_data_loader
+from train import trainer
+
+
+parser = ArgumentParser(description="run the model")
+parser.add_argument("--model", type=str, default='model.pth')
+parser.add_argument("--root_data", type=str, default="COCOdataset/all2017")
+parser.add_argument("--ann_train", type=str, default="MLDL23_NAS_Project/visualwakewords/instances_train.json")
+parser.add_argument("--ann_val", type=str, default="MLDL23_NAS_Project/visualwakewords/instances_val.json")
+parser.add_argument("--batch_size", type=int, default=64)
+parser.add_argument("--learning_rate", type=float, default=0.1)
+parser.add_argument("--momentum", type=float, default=0.9)
+parser.add_argument("--epochs", type=int, default=10)
+parser.add_argument("--weight_decay", type=float, default=0.000001)
+
+
+args = parser.parse_args()
+model = torch.load(args.model)
+
+
+root_data = args.root_data
+path_annotations_train = args.ann_train
+path_annotations_val = args.ann_val
+
+# batch size = 64, 96, 128 (possible error CUDA out of memory)
+batch_size = args.batch_size
+learning_rate = args.learning_rate
+momentum = args.momentum
+epochs = args.epochs
+weigth_decay = args.weight_decay
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+
+train_dataloader, val_dataloader, test_dataloader = get_data_loader(root_data,
+                                                                        path_annotations_train,
+                                                                        path_annotations_val,
+                                                                        64)
+#inputs = next(iter(train_dataloader))
+
+MV2_val_loss, MV2_val_accuracy, MV2_train_loss, MV2_train_accuracy = trainer(train_dataloader,
+                                                                                 val_dataloader,
+                                                                                 test_dataloader,
+                                                                                 learning_rate=0.1,
+                                                                                 weight_decay=0.000001,
+                                                                                 momentum=0.9,
+                                                                                 epochs=2,
+                                                                                 model= model,
+                                                                                 device=device )
+    
+
